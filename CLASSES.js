d0df33ccc3dee6e6553daa7d12b2ac6339748ac5
Jaime Jacobo <jaimejacobordv@gmail.com>
2018-10-26 23:03:20 -0400

    updated

--- CLASSES.js
+++ CLASSES.js
@@ -146,3 +146,78 @@ class Item {
   console.log(cascos.calculatePrice());
   console.log(compra.calculatePrice());
   
+
+
+//EJEMPLO DE CÓMO FUNCIONA EL SUPER() EN LAS CLASES
+
+
+class Animal {
+
+  constructor(nameInt, ownerInt, soundInt){
+
+    this.name = nameInt;
+    this.owner = ownerInt;
+    this.sound = soundInt;
+  }
+}
+
+class Dog extends Animal {
+  //Este constructor() nos indica qué argumentos tenemos que añadir para agregar al objeto
+  constructor(nameInt, ownerInt){
+    //Este super() me está diciendo: estos dos valores que se están metiendo, míralos arriba y devuélveme los nombres para ponerlos dentro de la función. Solo va a coger los valores de arriba que se pongan aquí. 
+    super(nameInt, ownerInt);
+    //Ahora, añádeme estos también.
+    this.sound = "Guau Guau";
+    this.humanRelation = "love";
+  }
+}
+
+let myAnimal = new Animal("Arya", "Josephine", "--");
+let myDog = new Dog("Max",  "Owen");
+
+console.log(myDog);
+
+
+
+//EJEMPLO DE CÓMO, UTILIZANDO super(), LLAMAR A UNA FUNCIÓN QUE ESTÁ DENTRO DE UNA CLASE, DESDE OTRA CLASE.
+
+class Item {
+
+  constructor(nameInt, priceInt){
+    this.name = nameInt;
+    this.price = priceInt;
+  }
+
+  calculatePrice(){
+    if(this.name === 'fruit'){
+      return ((this.price*0.95).toFixed(2));
+    } else {
+      return this.price;
+    }
+  }
+}
+
+class Fruit extends Item {
+  //aquí van a ir todos los argumentos que hay que escribir en el nuevo objeto
+  constructor(nameInt, priceInt, expirationDateInt, brandInt){
+    //Este super() nos indica cuáles de los argumentos tenemos que cogerlos de Item
+    super(nameInt, priceInt);
+    //Estos elementos hay que añadirlos, como de costumbre
+    this.expirationDate = expirationDateInt;
+    this.brand = brandInt;
+  }
+
+  calculatePrice() {
+    //Aqui le indicamos que cuando ejecutemos la función, nos ejecute la funcion del "super", es decir, la de Item, y nos devuelva ese valor
+    return super.calculatePrice();
+  }
+}
+
+
+let cascos = new Item('Bosa', 700);
+let compra = new Item('fruit', 3);
+let banana = new Fruit('banana', 2, 'march', 'Canarias');
+
+console.log(cascos.calculatePrice());
+console.log(compra.calculatePrice());
+console.log(banana.calculatePrice());
